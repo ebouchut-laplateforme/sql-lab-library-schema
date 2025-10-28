e5d51a559f28bfd4ee8f790042dabb63de4a2b8e
Eric Bouchut <ebouchut@gmail.com>
2025-10-28 10:11:34 +0100

    feat(db): Add a CSV to populate the authors_simple table
    Replaced Windows line-endings (CR+LF) with Unix line-endings (LF)
    
    with: dos2unix 20251028095800-add_table_authors_simple.import.csv

--- config/database/imports/20251027142700-books_simple.import.sh
+++ config/database/imports/20251027142700-books_simple.import.sh
@@ -14,25 +14,28 @@
 # The CSV file requires processing data before they can be inserted:
 # - We do not insert the first line because it is a header (not data).
 # - Each row contains 2 columns that we need to concatenate before insertion.
+#
+# See: https://github.com/ebouchut-laplateforme/sql-lab-library-schema/issues/7
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # TODO: Ensure that we have at least one file passed-in as argument or else display the command usage
 CSV_FILE=${1:-20251027142700-books_simple.import.csv}
 
 
-# `tail -n +2` Skips the header line (first line) and prints the content of the CSV file starting from line 2.
 # `awk -F` tells awk that the field separator is a comma.
 # Note: \047 is an escape sequence that denotes the simple quote (`'`).
 #       This trick made me bang my head against the wall ;-)!.
 #
-tail -n +2 "$CSV_FILE" |\
-  awk -F, '\
-    BEGIN {\
-      print "BEGIN;\n";\
-      print "USE library;\n";\
-      }\
-    { print "INSERT INTO books_simple (title) VALUES (\047" $0 "\047);"; }
-    END {\
-      print "\nCOMMIT;";\
+awk  '\
+  BEGIN {\
+    print "BEGIN;\n";\
+    print "USE library;\n";\
     }\
-  '
+  NR >= 2 {\
+    gsub(/\047/, "\047\047", $0);\
+    print "INSERT INTO books_simple (title) VALUES (\047" $0 "\047);"; 
+  }\
+  END {\
+    print "\nCOMMIT;";\
+  }\
+' <  "$CSV_FILE"
