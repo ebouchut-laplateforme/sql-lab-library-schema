f9dcd8a21f10b7aa31cd3fcb500908afe9e3c96a
Eric Bouchut <ebouchut@gmail.com>
2025-10-29 09:50:56 +0100

    feat(db): Add a shell script to read a CSV file to populate the authors_simple table

--- config/database/imports/20251028095800-authors_simple.import.sh
+++ config/database/imports/20251028095800-authors_simple.import.sh
@@ -24,9 +24,11 @@
 CSV_FILE=${1:-20251028095800-authors_simple.import.csv}
 
 
-# `tail -n +2 ` prints all lines except the first one (header), (starts printing at line 2)
 # `awk -F,` tells awk that the field separator is a comma.
 # Note: \047 is an escape sequence that denotes the simple quote (`'`).
+# `<<-'END_AWK'` is a bash here-doc notation for a multiline string where variables are not interpolated.
+# The purpose of using `previous_full_name` trick is to end each row/value with a comma except the last one.
+#
 awk -F, -f - "$CSV_FILE"  <<-'END_AWK'
   BEGIN {
     print "BEGIN;\n";
@@ -34,25 +36,55 @@ awk -F, -f - "$CSV_FILE"  <<-'END_AWK'
     print "INSERT INTO authors_simple (title) VALUES";
   }
   NR == 1 {
+    # Ignore the first line (header)
     next
   }
   NR > 2 {
-    print "\t" previous_line ",";
+    if (length(previous_full_name) > 0) {
+      # Note there is a comma after the value
+      print "\t('" previous_full_name "'),";
+    }
   }
   {
-    # Escape single quotes: Replace all occurrences of a single quote with 2 consecutive single quotes in the first 2 fields
-    gsub(/\047/, "\047\047", $1);
-    gsub(/\047/, "\047\047", $2);
+    # Create a variable with the full name  enclosed in single quotes, ready to be printed when processing the next line.
 
-    # Left-trim the leading space from the first name (second field: $2)
-    gsub(/^ /, "", $2);
+    # Escape single quotes: Replace all occurrences of a single quote with 2 consecutive single quotes in the the line (all fields)
+    gsub(/'/, "''", $0);
 
-    previous_line = "\t(\047" $2 " " $1 "\047)";
+    # Remove leading and trailing spaces from the last name ($1)
+    sub(/^ +/, "", $1);
+    sub(/ +$/, "", $1);
+
+    # Build the full name by concatenating the first name ($2) and last name ($1) separated with a space.
+    previous_full_name = "";
+
+    if (NF >= 2 && length($2) > 0) {
+      # The last name (second field: $2) is present.
+
+      # Remove leading and trailing spaces from the first name ($2)
+      sub(/^ +/, "", $2);
+      sub(/ +$/, "", $2);
+
+      previous_full_name = $2;
+    }
+    if (length($1) > 0) {
+      # The fist name ($1) is present
+      if (NR >= 2) {
+        # Only add a space between the last and full name when both are present.
+        previous_full_name = previous_full_name " "
+      }
+
+      # Add the first name if present
+      previous_full_name = previous_full_name $1
+    }
   }
   END {
-      print "\t" previous_line;
+    if (length(previous_full_name) > 0) {
+      # Note: No comma after the last value (last line)
+      print "\t('" previous_full_name "')";
+    }
 
-      print ");\n";
-      print "COMMIT;\n";
+    print ");\n";
+    print "COMMIT;\n";
   }
 END_AWK
